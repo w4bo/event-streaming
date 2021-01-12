f115c467dad3c4d7a3770f456328c94dd5d8e362
mfrancia <matteo.francia2@studio.unibo.it>
2021-01-12 14:16:02 +0100

    fix consumer

--- src/main/java/it/unibo/big/HelloConsumer.java
+++ src/main/java/it/unibo/big/HelloConsumer.java
@@ -5,16 +5,22 @@ import org.apache.kafka.clients.consumer.KafkaConsumer;
 
 import java.util.Arrays;
 import java.util.Properties;
+import java.util.UUID;
 
 public class HelloConsumer {
     public static void main(String[] args) {
         final Properties props = new Properties();
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, UUID.randomUUID().toString());
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
         props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("auto.offset.reset", "earliest");
         try (KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props)) {
-            consumer.subscribe(Arrays.asList("foo", "sample"));
-            consumer.poll(0).forEach(System.out::println);
+            consumer.subscribe(Arrays.asList("foo"));
+            System.out.println("Submitted");
+            while(true) {
+                consumer.poll(0).forEach(m -> System.out.println("Got message " + m.value()));
+            }
         } catch (final Exception e) {
             e.printStackTrace();
         }
